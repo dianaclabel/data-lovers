01ead843daf21f37f83fbf368118dcbe046d34d6
Dorianys Aponte <dorianysa@gmail.com>
2023-04-12 00:19:43 -0400

    se agrega estructura de la pagina secundaria nav, titulos, barra de busqueda, fondo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { example } from "./data.js";
+//import { example } from "./data.js";
 // import data from './data/lol/lol.js';
-import data from "./data/harrypotter/harrypotter.js";
+//import data from "./data/harrypotter/harrypotter.js";
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
+
+const divHechizos=document.querySelector("#div-hechizos");
+
+divHechizos.addEventListener("click",()=>{ 
+    document.location = "spell.html";
+});
