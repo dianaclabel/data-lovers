d86cdfa7423fd2c98301b9e897268735434ed38e
Dorianys Aponte <dorianysa@gmail.com>
2023-04-13 18:26:31 -0400

    se realizaron cambios en la interfaz principal se coloco un ancla para spell y potions

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
-//import { example } from "./data.js";
-// import data from './data/lol/lol.js';
-//import data from "./data/harrypotter/harrypotter.js";
-// import data from './data/rickandmorty/rickandmorty.js';
 
-//console.log(example, data);
+import { filterSpells } from "./data.js";
+const divHechizos = document.querySelector("#div-hechizos");
+const inputBuscador = document.querySelector("#input-buscador");
 
-const divHechizos=document.querySelector("#div-hechizos");
+if(divHechizos!==null){
+    divHechizos.addEventListener("click",()=>{ 
+        document.location = "spell.html";
+    });
+}
 
-divHechizos.addEventListener("click",()=>{ 
-    document.location = "spell.html";
-});
+if(inputBuscador!==null){
+    inputBuscador.addEventListener("keydown", (e) => {
+        const contenedorResultado = document.querySelector("#resultado-busqueda");
+        const spellEncontrados = filterSpells(inputBuscador.value);
+
+        contenedorResultado.innerHTML = "";
+        spellEncontrados.forEach(spell => {
+            const contenedorSpell  = document.createElement('div');
+            contenedorSpell.classList.add("spell-item")
+            contenedorSpell.innerHTML = `<h3>Nombre: ${spell.name}</h3><p>Descripcion: ${spell.description}</p><p>Tipo de hechizo: ${spell.spell_type}</p>`;
+            contenedorResultado.appendChild(contenedorSpell);
+        });
+        
+    });
+}
