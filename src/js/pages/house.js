c8d0ea74571567e5766943e5db7a4d4ba4756289
Diana Clabel <dianaclabel08@gmail.com>
2023-04-13 11:35:04 -0500

    se modifica house

--- src/js/pages/house.js
+++ src/js/pages/house.js
@@ -1,24 +1,20 @@
-// import { filterData } from "../data.js";
+import { filterData } from "../data.js";
 
-// import data from "../../data/harrypotter/data.js";
+import data from "../../data/harrypotter/data.js";
 
-// const house = document.getElementById("house").value;
-// const buscadorInput = document.querySelector("#buscador-house");
-// //buscar a los personajes por cada letra que esta en el buscador
+const house = document.getElementById("house").value;
+const buscadorInput = document.querySelector("#buscador-house");
+//buscar a los personajes por cada letra que esta en el buscador
+function buscarPersonajes() {
+  buscadorInput.addEventListener("keyup", (e) => {
+    const valorBuscador = buscadorInput.value;
+    console.log(valorBuscador);
+  });
+}
 
-// console.log("estas " + house);
+function creacionCard() {
+  let cardEstudiantesDiv = document.createElement("div");
+  cardEstudiantesDiv.classList.add("card-estudiante");
+}
 
-// function buscarPersonajes(data) {
-//   buscadorInput.addEventListener("keyup", (e) => {
-//     let valorBuscador = buscadorInput.value;
-//     console.log(valorBuscador);
-//     console.log(house);
-//   });
-// }
-
-// function creacionCard() {
-//   let cardEstudiantesDiv = document.createElement("div");
-//   cardEstudiantesDiv.classList.add("card-estudiante");
-// }
-
-// buscarPersonajes(data);
+buscarPersonajes(data);
