7e38ea9e3e1c99aca08a20b854fd219d1bd55ea9
Diana Clabel <dianaclabel08@gmail.com>
2023-04-27 00:18:40 -0500

    se realiza las pruebas unitarias

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,4 +1,12 @@
-import { computeStats, filterData, filterGender } from "../src/js/data.js";
+import {
+  computeStats,
+  dataNameToLowerCase,
+  filterData,
+  filterGender,
+  filterBooks,
+  filterTypeAncestry,
+  orderName,
+} from "../src/js/data.js";
 
 const characters = [
   { name: "Juan", gender: "Male", house: "Gryffindor", ancestry: "Pure-blood" },
@@ -9,8 +17,24 @@ const characters = [
     house: "Gryffindor",
     ancestry: "Pure-blood",
   },
+  {
+    name: "Mariano",
+    gender: "Male",
+    house: "Slytherin",
+    ancestry: "Half-bood",
+  },
   { name: "Maria", gender: "Female", house: "Ravenclaw", ancestry: "Muggle" },
+  { name: "Ana", gender: "Female", house: "Slytherin", ancestry: "Half-bood" },
+  {
+    name: "Rosa",
+    gender: "Female",
+    house: "Hufflepuff",
+    ancestry: "Half-bood",
+  },
+  { name: "Piero", gender: "Male", house: "Hufflepuff", ancestry: "Muggle" },
 ];
+
+const books = [{ title: "Los tres cerditos" }, { title: "La cenicienta" }];
 
 const charactersGryffindor = [
   { name: "Juan", gender: "Male", house: "Gryffindor", ancestry: "Pure-blood" },
@@ -22,6 +46,16 @@ const charactersGryffindor = [
   },
 ];
 
+const charactersSlytherin = [
+  {
+    name: "Mariano",
+    gender: "Male",
+    house: "Slytherin",
+    ancestry: "Half-bood",
+  },
+  { name: "Ana", gender: "Female", house: "Slytherin", ancestry: "Half-bood" },
+];
+
 const charactersMale = [
   { name: "Juan", gender: "Male", house: "Gryffindor", ancestry: "Pure-blood" },
   {
@@ -30,22 +64,85 @@ const charactersMale = [
     house: "Gryffindor",
     ancestry: "Pure-blood",
   },
+  {
+    name: "Mariano",
+    gender: "Male",
+    house: "Slytherin",
+    ancestry: "Half-bood",
+  },
+  { name: "Piero", gender: "Male", house: "Hufflepuff", ancestry: "Muggle" },
 ];
+
+const charactersFemale = [
+  { name: "Carla", gender: "Female", house: "Ravenclaw", ancestry: "Muggle" },
+  { name: "Maria", gender: "Female", house: "Ravenclaw", ancestry: "Muggle" },
+  { name: "Ana", gender: "Female", house: "Slytherin", ancestry: "Half-bood" },
+  {
+    name: "Rosa",
+    gender: "Female",
+    house: "Hufflepuff",
+    ancestry: "Half-bood",
+  },
+];
+
+const charactersMuggle = [
+  { name: "Carla", gender: "Female", house: "Ravenclaw", ancestry: "Muggle" },
+  { name: "Maria", gender: "Female", house: "Ravenclaw", ancestry: "Muggle" },
+  { name: "Piero", gender: "Male", house: "Hufflepuff", ancestry: "Muggle" },
+];
+
+const charactersPureBlood = [
+  { name: "Juan", gender: "Male", house: "Gryffindor", ancestry: "Pure-blood" },
+  {
+    name: "Rodrigo",
+    gender: "Male",
+    house: "Gryffindor",
+    ancestry: "Pure-blood",
+  },
+];
+const charactersOrderName = [
+  { name: "Ana", gender: "Female", house: "Slytherin", ancestry: "Half-bood" },
+  { name: "Carla", gender: "Female", house: "Ravenclaw", ancestry: "Muggle" },
+  { name: "Juan", gender: "Male", house: "Gryffindor", ancestry: "Pure-blood" },
+  { name: "Maria", gender: "Female", house: "Ravenclaw", ancestry: "Muggle" },
+  {
+    name: "Mariano",
+    gender: "Male",
+    house: "Slytherin",
+    ancestry: "Half-bood",
+  },
+  { name: "Piero", gender: "Male", house: "Hufflepuff", ancestry: "Muggle" },
+  {
+    name: "Rodrigo",
+    gender: "Male",
+    house: "Gryffindor",
+    ancestry: "Pure-blood",
+  },
+  {
+    name: "Rosa",
+    gender: "Female",
+    house: "Hufflepuff",
+    ancestry: "Half-bood",
+  },
+];
+
+const nameCharacter = {
+  name: "Juan",
+  gender: "Male",
+  house: "Gryffindor",
+  ancestry: "Pure-blood",
+};
+
+const nameToLowerCase = "juan";
 
 describe("test function for computeStats", () => {
   it("counts characters for each house", () => {
     expect(computeStats({ characters })).toEqual({
-      hufflepuff: 0,
+      hufflepuff: 2,
       gryffindor: 2,
       ravenclaw: 2,
-      slytherin: 0,
+      slytherin: 2,
     });
-  });
-});
-
-describe("test function gender", () => {
-  it("Filter male", () => {
-    expect(filterGender(characters, "Male")).toEqual(charactersMale);
   });
 });
 
@@ -57,7 +154,54 @@ describe("test function for character of each house", () => {
   });
 });
 
-//   it("returns `anotherExample`", () => {
-//     expect(anotherExample()).toBe("OMG");
-//   });
-// });
+describe("test function for character of each house", () => {
+  it("function for characters of slytherin", () => {
+    expect(filterData({ characters }, "Slytherin")).toEqual(
+      charactersSlytherin
+    );
+  });
+});
+
+describe("test function for lowercase name", () => {
+  it("function for lowercase character name", () => {
+    expect(dataNameToLowerCase(nameCharacter)).toEqual(nameToLowerCase);
+  });
+});
+
+describe("test function gender", () => {
+  it("Filter male", () => {
+    expect(filterGender(characters, "Male")).toEqual(charactersMale);
+  });
+});
+
+describe("test function gender", () => {
+  it("Filter Female", () => {
+    expect(filterGender(characters, "Female")).toEqual(charactersFemale);
+  });
+});
+
+describe("test function ancestry", () => {
+  it("Filter muggle", () => {
+    expect(filterTypeAncestry(characters, "Muggle")).toEqual(charactersMuggle);
+  });
+});
+
+describe("test function ancestry", () => {
+  it("Filter pure-blood", () => {
+    expect(filterTypeAncestry(characters, "Pure-blood")).toEqual(
+      charactersPureBlood
+    );
+  });
+});
+
+describe("test function order name", () => {
+  it("order A-Z", () => {
+    expect(orderName(characters)).toEqual(charactersOrderName);
+  });
+});
+
+describe("test function books", () => {
+  it("Filter books", () => {
+    expect(filterBooks({ books })).toEqual(books);
+  });
+});
